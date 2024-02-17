4b728a92dda9555f7ad4c9a0bc4205e445fc748c
Konrad Micek <98konrad98@gmail.com>
2024-02-17 14:18:53 +0100

    Day01 Part2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System.Text;
 
-ulong result = 0;
+Dictionary<string, uint> wordsNumbers = new()
+{
+    { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, {"five", 5 }, {"six", 6 }, {"seven", 7 }, {"eight", 8}, {"nine", 9}
+};
+
+ulong resultOnlyDigits = 0;
+ulong resultWithWords = 0;
+
 string fileName = "input.txt";
 using (var fileStream = File.OpenRead(fileName))
 using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
@@ -9,19 +16,49 @@ using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
     while ((line = streamReader.ReadLine()) != null)
     {
         List<uint> numbersInLine = new();
+        List<uint> numbersAndWordsInLine = new();
+
+        string remainLetters = "";
         foreach (char character in line)
         {
             if (uint.TryParse(character.ToString(), out uint number))
+            {
                 numbersInLine.Add(number);
+                numbersAndWordsInLine.Add(number);
+                remainLetters = "";
+            }
+            else
+            {
+                remainLetters += character;
+
+                if (remainLetters.Length < 3)
+                    continue;
+
+                foreach (var wordNumber in wordsNumbers)
+                {
+                    if (remainLetters.EndsWith(wordNumber.Key))
+                    {
+                        numbersAndWordsInLine.Add(wordNumber.Value);
+                        break;
+                    }
+                }
+            }
         }
 
-        if (numbersInLine.Count > 0)
-        {
-            uint number1 = numbersInLine.First();
-            uint number2 = numbersInLine.Last();
-            result += (10 * number1) + number2;
-        }
+        resultOnlyDigits += GetLineResult(numbersInLine);
+        resultWithWords += GetLineResult(numbersAndWordsInLine);
     }
 }
 
-Console.WriteLine(result);
+Console.WriteLine($"Result from only digits: {resultOnlyDigits}");
+Console.WriteLine($"Result with words included: {resultWithWords}");
+
+static uint GetLineResult(List<uint> values)
+{
+    if (values.Count == 0)
+        return 0;
+
+    uint number1 = values.First();
+    uint number2 = values.Last();
+    return (10 * number1) + number2;
+}
