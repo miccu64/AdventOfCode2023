85802901ce61b0090c902b3515c83fefc302a3ee
Konrad Micek <98konrad98@gmail.com>
2024-02-19 17:22:15 +0100

    Day04 part1

---
+++ Day04/Models/Card.cs
@@ -0,0 +1,29 @@
+﻿namespace Day04.Models
+{
+    internal class Card
+    {
+        public int Points { get; private set; }
+
+        public Card(string info)
+        {
+            string[] allNumbersInfo = info.Split(':')[1].Trim().Split('|');
+            string winningNumbersInfo = allNumbersInfo[0].Trim();
+            string cardNumbersInfo = allNumbersInfo[1].Trim();
+
+            List<int> winningNumbers = ParseNumbers(winningNumbersInfo);
+            List<int> cardNumbers = ParseNumbers(cardNumbersInfo);
+
+            List<int> matchNumbers = winningNumbers.Intersect(cardNumbers).ToList();
+            Points = matchNumbers.Count > 0 ?
+                (int)Math.Pow(2, matchNumbers.Count - 1) :
+                0;
+        }
+
+        private List<int> ParseNumbers(string info)
+        {
+            return info.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day04.Models;
+
+long testResult = 0;
+string[] testData = File.ReadAllLines("test.txt");
+foreach (string line in testData)
+    testResult += new Card(line).Points;
+
+Console.WriteLine($"Test - expected: 13, got: {testResult}\n");
+
+long result = 0;
+string[] data = File.ReadAllLines("input.txt");
+foreach (string line in data)
+    result += new Card(line).Points;
+
+Console.WriteLine($"Result: {result}");
