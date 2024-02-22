1c002deccd177dbf21022f7a4867ee1c48a19950
Konrad Micek <98konrad98@gmail.com>
2024-02-22 22:46:47 +0100

    Day05 init

---
+++ Day05/Models/Almanac.cs
@@ -0,0 +1,25 @@
+﻿namespace Day05.Models
+{
+    internal class Almanac
+    {
+        public readonly Dictionary<int, Map> Maps = new();
+
+        public readonly List<int> Seeds;
+
+        public Almanac(string fileName) { 
+            string[] data = File.ReadAllLines(fileName);
+
+            string[] seedData = data[0].Split(' ')[1..];
+            Seeds = seedData.Select(int.Parse).ToList();
+
+            int currentStart = 2;
+            for(int i = currentStart; i<data.Length; i++) {
+                if (!string.IsNullOrEmpty(data[i]))
+                    continue;
+
+
+
+            }
+        }
+    }
+}
---
+++ Day05/Models/Map.cs
@@ -0,0 +1,12 @@
+﻿namespace Day05.Models
+{
+    internal class Map
+    {
+        public string Name { get; private set; }
+        public readonly Dictionary<int, int> SourceToDestination = new();
+
+        public Map(string[] data) { 
+        
+        }
+    }
+}
