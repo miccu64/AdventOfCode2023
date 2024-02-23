263043c17f84f17107acca721df99887b3849e6d
Konrad Micek <98konrad98@gmail.com>
2024-02-23 11:06:49 +0100

    Day05 part2 v1 - too long execution time

---
+++ Day05/Models/AlmanacPart2.cs
@@ -0,0 +1,33 @@
+﻿namespace Day05.Models
+{
+    internal class AlmanacPart2 : Almanac
+    {
+        public AlmanacPart2(string fileName) : base(fileName) { }
+
+        public new ulong FindLowestLocationNumber()
+        {
+            ulong lowestLocationNumber = ulong.MaxValue;
+
+            for (int i = 0; i < Seeds.Count; i += 2)
+            {
+                ulong currentSeed = Seeds[i];
+                ulong rangeLength = Seeds[i + 1];
+                ulong rangeEnd = currentSeed + rangeLength - 1;
+
+                while (currentSeed <= rangeEnd)
+                {
+                    ulong latestValue = currentSeed;
+                    foreach (Map map in Maps)
+                        latestValue = map.GetDestinationValue(latestValue);
+
+                    if (latestValue < lowestLocationNumber)
+                        lowestLocationNumber = latestValue;
+
+                    currentSeed++;
+                }
+            }
+
+            return lowestLocationNumber;
+        }
+    }
+}
--- Day05/Program.cs
+++ Day05/Program.cs
@@ -8,3 +8,12 @@ Almanac almanac = new("input.txt");
 ulong resultPart1 = almanac.FindLowestLocationNumber();
 Console.WriteLine($"Part 1 - got: {resultPart1}");
 
+
+
+AlmanacPart2 testAlmanacPart2 = new("test.txt");
+ulong testResultPart2 = testAlmanacPart2.FindLowestLocationNumber();
+Console.WriteLine($"Test part 2 - expected value: 46, got: {testResultPart2}");
+
+AlmanacPart2 almanacPart2 = new("input.txt");
+ulong resultPart2 = almanacPart2.FindLowestLocationNumber();
+Console.WriteLine($"Part 2 - got: {resultPart2}");
