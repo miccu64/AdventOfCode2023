36db87a4be1b5af62fed0b9e2b4446b087ea3c74
Konrad Micek <98konrad98@gmail.com>
2024-03-01 11:24:47 +0100

    Day08 part2 final

--- Day08/ModelsPart2/NetworkPart2.cs
+++ Day08/ModelsPart2/NetworkPart2.cs
@@ -8,25 +8,50 @@ namespace Day08.ModelsPart2
 
         public override long Iterate()
         {
-            long stepsCount = 0;
-            int directionIndex = 0;
-            List<string> currentPositions = Nodes.Where(node => node.Start.EndsWith('A'))
+            List<string> starts = Nodes.Where(node => node.Start.EndsWith('A'))
                 .Select(node => node.Start)
                 .ToList();
+            Dictionary<string, Dictionary<string, long>> startsWithEnds = new();
 
-            while (!currentPositions.All(pos => pos.EndsWith('Z')))
+            foreach (string start in starts)
             {
-                char direction = Directions[directionIndex];
-                List<Node> currentNodes = Nodes.Where(node => currentPositions.Contains(node.Start)).ToList();
-                currentPositions = currentNodes.Select(node => node.FindNextNodeName(direction)).ToList();
+                long stepsCount = 0;
+                int directionIndex = 0;
+                Dictionary<string, long> endsWithSteps = new();
+                string currentPosition = start;
 
-                stepsCount++;
-                directionIndex++;
-                if (directionIndex == Directions.Length)
-                    directionIndex = 0;
+                do
+                {
+                    char direction = Directions[directionIndex];
+                    Node currentNode = Nodes.Single(node => node.Start == currentPosition);
+                    currentPosition = currentNode.FindNextNodeName(direction);
+
+                    stepsCount++;
+                    directionIndex++;
+                    if (directionIndex == Directions.Length)
+                        directionIndex = 0;
+
+                    if (endsWithSteps.ContainsKey(currentPosition))
+                        break;
+
+                    if (currentPosition.EndsWith('Z'))
+                        endsWithSteps[currentPosition] = stepsCount;
+                }
+                while (currentPosition != start);
+
+                startsWithEnds[start] = endsWithSteps;
             }
 
-            return stepsCount;
+            long[] allLengths = startsWithEnds.SelectMany(x => x.Value).Select(x => x.Value).ToArray();
+            return LeastCommonMultiple(allLengths);
+        }
+
+        static long LeastCommonMultiple(long[] numbers)
+        {
+            static long greatestCommonDivisor(long a, long b) => b == 0 ? a : greatestCommonDivisor(b, a % b);
+            static long lcm(long a, long b) => Math.Abs(a * b) / greatestCommonDivisor(a, b);
+
+            return numbers.Aggregate(lcm);
         }
     }
 }
