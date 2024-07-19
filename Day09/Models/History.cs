c759e0dab39e9b741787fc9e914ab2349db4b854
Konrad Micek <98konrad98@gmail.com>
2024-07-19 19:55:21 +0200

    Day09 Part 1

---
+++ Day09/Models/History.cs
@@ -0,0 +1,45 @@
+﻿namespace Day09.Models
+{
+    internal class History
+    {
+        private readonly List<List<int>> Differences;
+
+        public History(string data)
+        {
+            List<int> firstLine = data.Split(' ').Select(int.Parse).ToList();
+            Differences = new() { firstLine };
+        }
+
+        public int GetLatestValue()
+        {
+            DoDifferences();
+            Extrapolate();
+            return Differences.First().Last();
+        }
+
+        private void DoDifferences()
+        {
+            List<int> line = Differences.Last();
+            do
+            {
+                List<int> leftValues = line.SkipLast(1).ToList();
+                List<int> rightValues = line.Skip(1).ToList();
+                line = leftValues.Zip(rightValues, (left, right) => right - left).ToList();
+                Differences.Add(line);
+            }
+            while (line.Any(x => x != 0));
+        }
+
+        private void Extrapolate()
+        {
+            Differences.Last().Add(0);
+
+            for (int i = Differences.Count - 1; i > 0; i--)
+            {
+                List<int> lineLower = Differences[i];
+                List<int> lineUpper = Differences[i - 1];
+                lineUpper.Add(lineLower.Last() + lineUpper.Last());
+            }
+        }
+    }
+}
---
+++ Day09/Program.cs
@@ -0,0 +1,22 @@
+﻿using Day09.Models;
+
+static long GetResult(string fileName)
+{
+    long result = 0;
+    List<string> lines = File.ReadLines(fileName).ToList();
+    foreach (string line in lines)
+    {
+        result += new History(line).GetLatestValue();
+    }
+    return result;
+}
+
+string testFile = "test1.txt";
+long testResult = GetResult(testFile);
+Console.WriteLine("Test result: " + testResult);
+
+string file = "input.txt";
+long result = GetResult(file);
+Console.WriteLine("Result: " + result);
+
+
