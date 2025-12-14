6ccf88d059be3f993986925e4fd614bfee263def
miccu64 <98konrad98@gmail.com>
2025-12-14 16:30:56 +0100

    Day13 part1 final

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -4,3 +4,6 @@ using Day13.Models;
 
 int testResult1 = Pattern.GetSummary("TestData1.txt");
 Console.WriteLine("Part 1 test expected result: 405, got: " + testResult1);
+
+int result1 = Pattern.GetSummary("Input1.txt");
+Console.WriteLine("Part 1 result: " + result1);
