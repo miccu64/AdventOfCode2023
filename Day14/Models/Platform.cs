618669008e7e5de56ac065e3713d75aa8a2071d5
miccu64 <98konrad98@gmail.com>
2025-12-20 14:01:30 +0100

    Day14 refactor

--- Day14/Models/Platform.cs
+++ Day14/Models/Platform.cs
@@ -2,54 +2,76 @@ namespace Day14.Models;
 
 public class Platform
 {
-    private readonly CellType[,] _cells;
+    protected readonly CellType[,] Cells;
+    protected readonly int XLength;
+    protected readonly int YLength;
 
     public Platform(string fileName)
     {
         string[] lines = File.ReadAllLines(fileName);
-        _cells = new CellType[lines.Length, lines.First().Length];
+        Cells = new CellType[lines.Length, lines.First().Length];
 
         for (int y = 0; y < lines.Length; y++)
         {
             string line = lines[y];
             for (int x = 0; x < line.Length; x++)
             {
-                _cells[y, x] = GetCellType(line[x]);
+                Cells[y, x] = GetCellType(line[x]);
             }
         }
+
+        XLength = Cells.GetLength(1);
+        YLength = Cells.GetLength(0);
     }
 
     public long GetWeightAfterTiltNorth()
     {
-        long weight = 0;
-        
-        int xLength = _cells.GetLength(1);
-        int yLength = _cells.GetLength(0);
+        TiltNorth();
+        return GetWeight();
+    }
 
-        for (int x = 0; x < xLength; x++)
+    protected long GetWeight()
+    {
+        long weight = 0;
+
+        for (int x = 0; x < XLength; x++)
+        {
+            for (int y = 0; y < YLength; y++)
+            {
+                CellType cell = Cells[y, x];
+                if (cell == CellType.MovableRock)
+                    weight += YLength - y;
+            }
+        }
+
+        return weight;
+    }
+
+    protected void TiltNorth()
+    {
+        for (int x = 0; x < XLength; x++)
         {
             int latestUsedY = -1;
-            for (int y = 0; y < yLength; y++)
+            for (int y = 0; y < YLength; y++)
             {
-                CellType cell =  _cells[y, x];
+                CellType cell = Cells[y, x];
                 switch (cell)
                 {
                     case CellType.Empty:
                         continue;
                     case CellType.MovableRock:
                         latestUsedY++;
-                        weight += yLength - latestUsedY;
+                        Cells[y, x] = CellType.Empty;
+                        Cells[latestUsedY, x] = CellType.MovableRock;
                         break;
                     case CellType.FixedRock:
                         latestUsedY = y;
                         break;
                     default:
-                        throw new ArgumentOutOfRangeException();
+                        throw new ArgumentException("Not supported cell type");
                 }
-            }  
+            }
         }
-
-        return  weight;
     }
 
     private static CellType GetCellType(char c)
---
+++ Day14/Models/PlatformPart2.cs
@@ -0,0 +1,8 @@
+namespace Day14.Models;
+
+public class PlatformPart2:Platform
+{
+    public PlatformPart2(string fileName) : base(fileName)
+    {
+    }
+}
