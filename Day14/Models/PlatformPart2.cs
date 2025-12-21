fe27298cd86aa6fed8f13e360dacd80262c84938
miccu64 <98konrad98@gmail.com>
2025-12-21 12:40:37 +0100

    Day14 part2 working

--- Day14/Models/PlatformPart2.cs
+++ Day14/Models/PlatformPart2.cs
@@ -2,11 +2,13 @@ namespace Day14.Models;
 
 public class PlatformPart2(string fileName) : Platform(fileName)
 {
+    private readonly List<long> _results = [];
     private TiltDirection _nextTiltDirection = TiltDirection.North;
 
     public long GetWeightAfterCycles(long cycles)
     {
-        for (long i = 0; i < cycles; i++)
+        const int patternSearchInterval = 1000;
+        for (long i = 1; i <= cycles; i++)
         {
             TiltNorth();
             _nextTiltDirection = TiltDirection.West;
@@ -16,6 +18,18 @@ public class PlatformPart2(string fileName) : Platform(fileName)
             _nextTiltDirection = TiltDirection.East;
             TiltNorth();
             _nextTiltDirection = TiltDirection.North;
+
+            _results.Add(GetWeight());
+
+            bool shouldCheckPatterns = i % patternSearchInterval == 0;
+            if (!shouldCheckPatterns)
+                continue;
+
+            long? resultFromPattern = FindResultByPattern(i, cycles);
+            if (resultFromPattern != null)
+            {
+                return resultFromPattern.Value;
+            }
         }
 
         return GetWeight();
@@ -23,17 +37,17 @@ public class PlatformPart2(string fileName) : Platform(fileName)
 
     protected override CellType GetCellForTilt(int y, int x)
     {
-        (int newY, int newX) = GetYXForTilt(y, x);
+        (int newY, int newX) = GetCoordinatesForTilt(y, x);
         return Cells[newY, newX];
     }
 
     protected override void SetCellForTilt(int y, int x, CellType cell)
     {
-        (int newY, int newX) = GetYXForTilt(y, x);
+        (int newY, int newX) = GetCoordinatesForTilt(y, x);
         Cells[newY, newX] = cell;
     }
 
-    private (int y, int x) GetYXForTilt(int y, int x)
+    private (int y, int x) GetCoordinatesForTilt(int y, int x)
     {
         // x1 = x * cos(fi) - y * sin(fi)
         // y1 = y * cos(fi) + x * sin(fi)
@@ -47,5 +61,29 @@ public class PlatformPart2(string fileName) : Platform(fileName)
             TiltDirection.East => (x, -y + shift),
             _ => throw new ArgumentException("Not supported tilt type")
         };
+    }
+
+    private long? FindResultByPattern(long currentCycles, long expectedCycles)
+    {
+        List<long> results = _results.ToList();
+        results.Reverse();
+
+        _results.Clear();
+
+        long latestResult = results[0];
+
+        int patternLength = results.IndexOf(latestResult, 1);
+        if (patternLength <= 0)
+            return null;
+
+        for (int i = 0; i < patternLength; i++)
+        {
+            bool patternInvalid = results[i] != results[i + patternLength];
+            if (patternInvalid)
+                return null;
+        }
+
+        int patternPosition = (int)((expectedCycles - currentCycles) % patternLength);
+        return results[patternPosition];
     }
 }
--- Day14/Program.cs
+++ Day14/Program.cs
@@ -10,3 +10,10 @@ PlatformPart2 testPlatform2 = new("TestData1.txt");
 Console.WriteLine($"Test 2 - result after 1 cycles: {testPlatform2.GetWeightAfterCycles(1)}, expected: 87");
 Console.WriteLine($"Test 2 - result after 2 cycles: {testPlatform2.GetWeightAfterCycles(1)}, expected: 69");
 Console.WriteLine($"Test 2 - result after 3 cycles: {testPlatform2.GetWeightAfterCycles(1)}, expected: 69");
+
+const long cycles = 1_000_000_000;
+PlatformPart2 testPlatform3 = new("TestData1.txt");
+Console.WriteLine($"Test 2 - result: {testPlatform3.GetWeightAfterCycles(cycles)}, expected: 64");
+
+PlatformPart2 platform2 = new("Input1.txt");
+Console.WriteLine($"Part 2 result: {platform2.GetWeightAfterCycles(cycles)}");
