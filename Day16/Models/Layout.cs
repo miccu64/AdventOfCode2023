9fff1749b9e3df4ed0ef4d77616e74f3f0190dce
miccu64 <98konrad98@gmail.com>
2026-07-11 09:53:00 +0200

    refactor: flipped x with y in coordinates

--- Day16/Models/Layout.cs
+++ Day16/Models/Layout.cs
@@ -8,15 +8,15 @@ public class Layout
     {
         string[] text = File.ReadAllLines(fileName);
 
-        int yLength = text.Length;
         int xLength = text[0].Length;
-        _layout = new Point[yLength, xLength];
+        int yLength = text.Length;
+        _layout = new Point[xLength, yLength];
 
         for (int y = 0; y < yLength; y++)
         {
             for (int x = 0; x < xLength; x++)
             {
-                _layout[y, x] = new Point(text[y][x]);
+                _layout[x, y] = new Point(text[x][y]);
             }
         }
     }
@@ -26,27 +26,27 @@ public class Layout
         Traverse(0, 0, Direction.Right);
     }
 
-    private void Traverse(int startY, int startX, Direction direction)
+    private void Traverse(int startX, int startY, Direction direction)
     {
     }
 
-    private (int y, int x) GetNextPointCoordinates(int y, int x, Direction direction)
+    private (int x, int y)? GetNextPointCoordinates(int x, int y, Direction direction)
     {
-        (int newY, int newX) newCoordinates = direction switch
+        (int x, int y) newCoordinates = direction switch
         {
-            Direction.Up => (y - 1, x),
-            Direction.Down => (y + 1, x),
-            Direction.Left => (y, x - 1),
-            Direction.Right => (y, x + 1),
+            Direction.Up => (x, y - 1),
+            Direction.Down => (x, y + 1),
+            Direction.Left => (x - 1, y),
+            Direction.Right => (x + 1, y),
             _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
         };
         try
         {
-            Point _ = _layout[y, x];
+            Point _ = _layout[newCoordinates.x, newCoordinates.y];
         }
         catch (IndexOutOfRangeException)
         {
-            return (-1, -1);
+            return null;
         }
 
         return newCoordinates;
