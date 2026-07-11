46c1c935e0ffe250edb86b0e05e10514401ed9c8
miccu64 <98konrad98@gmail.com>
2026-07-11 13:25:11 +0200

    refactor: cleanup traversing

--- Day16/Models/Layout.cs
+++ Day16/Models/Layout.cs
@@ -1,25 +1,8 @@
 namespace Day16.Models;
 
-public class Layout
+public class Layout(string fileName)
 {
-    private readonly Point[,] _layout;
-
-    public Layout(string fileName)
-    {
-        string[] text = File.ReadAllLines(fileName);
-
-        int xLength = text[0].Length;
-        int yLength = text.Length;
-        _layout = new Point[xLength, yLength];
-
-        for (int y = 0; y < yLength; y++)
-        {
-            for (int x = 0; x < xLength; x++)
-            {
-                _layout[x, y] = new Point(text[x][y]);
-            }
-        }
-    }
+    private readonly Point[,] _layout = LoadLayout(fileName);
 
     public int Traverse()
     {
@@ -28,66 +11,66 @@ public class Layout
         return _layout.Cast<Point>().Count(point => point.IsEnergized());
     }
 
+    private static Point[,] LoadLayout(string fileName)
+    {
+        string[] text = File.ReadAllLines(fileName);
+
+        int xLength = text[0].Length;
+        int yLength = text.Length;
+
+        Point[,] layout = new Point[xLength, yLength];
+        for (int y = 0; y < yLength; y++)
+        {
+            for (int x = 0; x < xLength; x++)
+            {
+                layout[x, y] = new Point(text[x][y]);
+            }
+        }
+
+        return layout;
+    }
+
     private void Traverse(int startX, int startY, Direction direction)
     {
         List<Task> pendingTraversions = [];
 
         (Point point, int x, int y)? nextPointInfo = TryGetNextPoint(startX, startY, direction);
-        while (nextPointInfo != null)
+        while (nextPointInfo is { } currentPoint)
         {
-            Console.WriteLine($"Current point: {nextPointInfo.Value.x}, {nextPointInfo.Value.y}, {direction}");
+            Console.WriteLine($"Current point: {currentPoint.x}, {currentPoint.y}, {direction}");
 
-            if (!nextPointInfo.Value.point.TryEnergize(direction))
+            if (!currentPoint.point.TryEnergize(direction))
                 break;
 
-            PointType nextPointType = nextPointInfo.Value.point.Type;
+            PointType nextPointType = currentPoint.point.Type;
 
-            bool noChanges = nextPointType == PointType.EmptySpace
-                             || (nextPointType == PointType.HorizontalSplitter &&
-                                 direction is Direction.Left or Direction.Right)
-                             || (nextPointType == PointType.VerticalSplitter &&
-                                 direction is Direction.Up or Direction.Down);
+            bool directionChangesNeeded = !(nextPointType == PointType.EmptySpace
+                                            || (nextPointType == PointType.HorizontalSplitter &&
+                                                direction is Direction.Left or Direction.Right)
+                                            || (nextPointType == PointType.VerticalSplitter &&
+                                                direction is Direction.Up or Direction.Down));
 
-            if (noChanges)
+            if (directionChangesNeeded)
             {
-                // nothing
-            }
-            else if (nextPointType == PointType.SlashMirror)
-            {
-                direction = direction switch
+                switch (nextPointType)
                 {
-                    Direction.Up => Direction.Right,
-                    Direction.Down => Direction.Left,
-                    Direction.Left => Direction.Down,
-                    Direction.Right => Direction.Up,
-                    _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
-                };
-            }
-            else if (nextPointType == PointType.BackslashMirror)
-            {
-                direction = direction switch
-                {
-                    Direction.Up => Direction.Left,
-                    Direction.Down => Direction.Right,
-                    Direction.Left => Direction.Up,
-                    Direction.Right => Direction.Down,
-                    _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
-                };
-            }
-            else if (nextPointType == PointType.HorizontalSplitter)
-            {
-                pendingTraversions.Add(TraverseTask(nextPointInfo.Value.x, nextPointInfo.Value.y, Direction.Right));
+                    case PointType.SlashMirror or PointType.BackslashMirror:
+                        direction = Reflect(nextPointType, direction);
+                        break;
+                    case PointType.HorizontalSplitter:
+                        pendingTraversions.Add(TraverseTask(currentPoint.x, currentPoint.y, Direction.Right));
 
-                direction = Direction.Left;
-            }
-            else if (nextPointType == PointType.VerticalSplitter)
-            {
-                pendingTraversions.Add(TraverseTask(nextPointInfo.Value.x, nextPointInfo.Value.y, Direction.Down));
+                        direction = Direction.Left;
+                        break;
+                    case PointType.VerticalSplitter:
+                        pendingTraversions.Add(TraverseTask(currentPoint.x, currentPoint.y, Direction.Down));
 
-                direction = Direction.Up;
+                        direction = Direction.Up;
+                        break;
+                }
             }
 
-            nextPointInfo = TryGetNextPoint(nextPointInfo.Value.x, nextPointInfo.Value.y, direction);
+            nextPointInfo = TryGetNextPoint(currentPoint.x, currentPoint.y, direction);
         }
 
         Task.WaitAll(pendingTraversions);
@@ -98,6 +81,21 @@ public class Layout
         Console.WriteLine($"Started new Task: {startX}, {startY}, {direction}");
         return Task.Run(() => Traverse(startX, startY, direction));
     }
+
+    private static Direction Reflect(PointType pointType, Direction direction) => (pointType, direction) switch
+    {
+        (PointType.SlashMirror, Direction.Up) => Direction.Right,
+        (PointType.SlashMirror, Direction.Down) => Direction.Left,
+        (PointType.SlashMirror, Direction.Left) => Direction.Down,
+        (PointType.SlashMirror, Direction.Right) => Direction.Up,
+
+        (PointType.BackslashMirror, Direction.Up) => Direction.Left,
+        (PointType.BackslashMirror, Direction.Down) => Direction.Right,
+        (PointType.BackslashMirror, Direction.Left) => Direction.Up,
+        (PointType.BackslashMirror, Direction.Right) => Direction.Down,
+
+        _ => throw new InvalidOperationException()
+    };
 
     private (Point point, int x, int y)? TryGetNextPoint(int x, int y, Direction direction)
     {
