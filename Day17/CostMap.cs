904d51fdd21a2f331ad66753c1e110c1db961a67
miccu64 <98konrad98@gmail.com>
2026-07-25 17:06:03 +0200

    fix: better iteration

--- Day17/CostMap.cs
+++ Day17/CostMap.cs
@@ -18,22 +18,32 @@ public class CostMap
     {
         DirectionCounter directionCounter = new(Direction.Right);
         PointInfo<Cell>? currentCellInfo = new(_grid[0, 0], 0, 0, Direction.Right);
+        currentCellInfo.Point.DistanceFromStart = 0;
 
         while (currentCellInfo != null)
         {
             PointInfo<Cell>? optimalCellInfo = _allDirections
-                .Select(direction => _grid.TryTraverse(currentCellInfo.X, currentCellInfo.Y, direction))
-                .Where(info =>
-                    info is { Point.IsVisited: false } && directionCounter.CanUseDirection(info.UsedDirection)
-                )
+                .Select(direction =>
+                {
+                    PointInfo<Cell>? cellInfo = _grid.TryTraverse(currentCellInfo.X, currentCellInfo.Y, direction);
+                    if (cellInfo == null || !directionCounter.CanUseDirection(cellInfo.UsedDirection))
+                        return null;
+
+                    int newDistance = currentCellInfo.Point.DistanceFromStart + cellInfo.Point.Cost;
+                    if (cellInfo.Point.DistanceFromStart > newDistance)
+                        cellInfo.Point.DistanceFromStart = newDistance;
+
+                    return cellInfo;
+                })
+                .Where(info => info is { Point.IsVisited: false })
                 .OrderBy(info => info!.Point.Cost)
                 .FirstOrDefault();
 
             if (optimalCellInfo != null)
             {
-                optimalCellInfo.Point.DistanceFromStart =
-                    optimalCellInfo.Point.Cost + currentCellInfo.Point.DistanceFromStart;
                 directionCounter.Update(optimalCellInfo.UsedDirection);
+                
+                currentCellInfo.Point.IsVisited = true;
             }
 
             currentCellInfo = optimalCellInfo;
@@ -55,7 +65,7 @@ public class CostMap
 
         public bool CanUseDirection(Direction direction)
         {
-            return Direction == direction && Counter < 3;
+            return direction != Direction || Counter < 3;
         }
 
         public void Update(Direction direction)
--- Day17/Models/Cell.cs
+++ Day17/Models/Cell.cs
@@ -2,9 +2,7 @@ namespace Day17.Models;
 
 public class Cell(char c)
 {
-    private const int UnvisitedDistance = -1;
-
     public int Cost { get; } = int.Parse(c.ToString());
-    public int DistanceFromStart { get; set; } = UnvisitedDistance;
-    public bool IsVisited => DistanceFromStart > UnvisitedDistance;
+    public int DistanceFromStart { get; set; } = int.MaxValue;
+    public bool IsVisited { get; set; }
 }
