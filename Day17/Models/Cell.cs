2b7e8c346ab6e4178ef6d6fc398d5ee9183dfe63
miccu64 <98konrad98@gmail.com>
2026-08-14 11:47:56 +0200

    refactor: removed wrong optimization attempt

--- Day17/Models/Cell.cs
+++ Day17/Models/Cell.cs
@@ -6,48 +6,9 @@ public class Cell(char c, Boundaries boundaries)
 {
     public int Cost { get; } = int.Parse(c.ToString());
     private List<TraversalInfo> _orderedTraversalInfos = [];
-    private const int MaxSingleCost = 9;
-    private int MaxPossible { get; } = MaxSingleCost * boundaries.MaxDirectionRepeats;
 
     public void AddRangeTraversalInfos(params TraversalInfo[] traversalInfos)
     {
-        // _orderedTraversalInfos = _orderedTraversalInfos
-        //     .Concat(traversalInfos)
-        //     .GroupBy(i => i.LatestDirection)
-        //     .SelectMany(g =>
-        //     {
-        //         List<TraversalInfo> ordered = g
-        //             .OrderBy(i => i.DistanceFromStart)
-        //             .ThenBy(i => i.LatestDirectionRepeats)
-        //             .ThenByDescending(i => i.IsVisited)
-        //             .ToList();
-        //
-        //         if (ordered.Count == 1)
-        //             return ordered;
-        //
-        //         List<TraversalInfo> relevantInfos =
-        //         [
-        //             ordered[0]
-        //         ];
-        //
-        //         ordered.RemoveAt(0);
-        //
-        //         int maxDistance = ordered[0].DistanceFromStart + MaxPossible;
-        //
-        //         foreach (TraversalInfo info in ordered)
-        //         {
-        //             // if (info.DistanceFromStart > maxDistance)
-        //             //     break;
-        //
-        //             if (info.LatestDirectionRepeats < relevantInfos.Last().LatestDirectionRepeats)
-        //                 relevantInfos.Add(info);
-        //         }
-        //
-        //         return relevantInfos;
-        //     })
-        //     .OrderBy(i => i.DistanceFromStart)
-        //     .ToList();
-        
         _orderedTraversalInfos = _orderedTraversalInfos
             .Concat(traversalInfos)
             .GroupBy(i => new { i.LatestDirection, i.LatestDirectionRepeats })
